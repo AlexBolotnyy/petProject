1f8d35a6e0fd53a3fae2e6afe8aaf0a44f8441ad
bolotnyi-ad <bolotnyi-ad@serviceit.su>
2022-06-28 13:06:14 +0400

    Проект запускается и выполняет операции с репозиторием. Нужно разобраться с Mapstruct

--- src/main/java/com/example/swaggertest/model/mapper/persone/PersonMapper.java
+++ src/main/java/com/example/swaggertest/model/mapper/persone/PersonMapper.java
@@ -1,18 +1,8 @@
 package com.example.swaggertest.model.mapper.persone;
 
-import com.example.swaggertest.entity.Person;
-import com.example.swaggertest.model.PersonOutputDto;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
-//
-//@Mapper(componentModel = "spring")
+
 public interface PersonMapper {
-//
-//    @Mapping(target = "id", source = "id")
-//    @Mapping(target = "firstName", source = "firstName")
-//    @Mapping(target = "secondName", source = "secondName")
-//    @Mapping(target = "age", source = "age")
-//    PersonOutputDto toDto(Person person);
+
 
 
 }
--- src/main/java/com/example/swaggertest/service/PersonServiceImpl.java
+++ src/main/java/com/example/swaggertest/service/PersonServiceImpl.java
@@ -3,10 +3,7 @@ package com.example.swaggertest.service;
 import com.example.swaggertest.entity.Person;
 import com.example.swaggertest.model.PersonDto;
 import com.example.swaggertest.model.PersonOutputDto;
-import com.example.swaggertest.model.mapper.persone.PersonMapper;
 import com.example.swaggertest.repository.PersonRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +12,17 @@ import org.springframework.stereotype.Service;
 public class PersonServiceImpl implements PersonService {
 
     private final PersonRepository personRepository;
-//    private final PersonMapper personMapper;
+
 
     @Override
     public PersonOutputDto getPersonDto(Integer id) {
-//        Person person = personRepository.findPersonById(id).orElse(new Person());
-//        return personMapper.toDto(person);
-        return null;
+        Person person = personRepository.findPersonById(id).orElse(new Person());
+        PersonOutputDto dto = new PersonOutputDto();
+        dto.setId(person.getId());
+        dto.setFirstName(person.getFirstName());
+        dto.setSecondName(person.getSecondName());
+        dto.setAge(person.getAge());
+        return dto;
     }
 
     @Override
